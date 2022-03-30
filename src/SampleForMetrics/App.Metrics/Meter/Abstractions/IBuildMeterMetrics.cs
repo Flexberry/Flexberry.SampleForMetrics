eaf7c0ece0a6fda9512f79bd777bde20fbdc7028
FCherepanov <FCherepanov@ics.perm.ru>
2022-03-30 13:46:07 +0500

    #226287 Добавлен форк AppMetrics

---
+++ src/SampleForMetrics/App.Metrics/Meter/Abstractions/IBuildMeterMetrics.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IBuildMeterMetrics.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using App.Metrics.Abstractions.MetricTypes;
+
+namespace App.Metrics.Meter.Abstractions
+{
+    public interface IBuildMeterMetrics
+    {
+        IMeterMetric Build(IClock clock);
+    }
+}
---
+++ src/SampleForMetrics/App.Metrics/Serialization/NoOpMetricDataSerializer.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="NoOpMetricDataSerializer.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using App.Metrics.Abstractions.Serialization;
+using App.Metrics.Core.Internal;
+
+namespace App.Metrics.Serialization
+{
+    [AppMetricsExcludeFromCodeCoverage]
+    public sealed class NoOpMetricDataSerializer : IMetricDataSerializer
+    {
+        public T Deserialize<T>(string value) { return default(T); }
+
+        public string Serialize<T>(T value) { return string.Empty; }
+    }
+}
