b6fb0258ae59ee9e2a90fd93cb162d8820a43dd8
FCherepanov <FCherepanov@ics.perm.ru>
2022-03-30 14:16:56 +0500

    #226287 Добавлены метрики.

---
+++ src/SampleForMetrics/ODataBackend/App_Start/WebApiConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace IIS.SampleForMetrics
+{
+    public static class WebApiConfig
+    {
+        public static void RegisterWebApi(this HttpConfiguration httpConfiguration)
+        {
+            httpConfiguration.MapHttpAttributeRoutes();
+        }
+    }
+}
--- src/SampleForMetrics/ODataBackend/Startup.cs
+++ src/SampleForMetrics/ODataBackend/Startup.cs
@@ -3,9 +3,16 @@
 [assembly: OwinStartup(typeof(IIS.SampleForMetrics.Startup))]
 namespace IIS.SampleForMetrics
 {
+    using App.Metrics;
+    using App.Metrics.Extensions.Owin.WebApi;
+    using App.Metrics.Reporting.Abstractions;
     using ICSSoft.STORMNET.Business;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Practices.Unity.Configuration;
     using Owin;
+    using System;
+    using System.Reflection;
+    using System.Web.Hosting;
     using System.Web.Http;
     using Unity;
 
@@ -17,6 +24,53 @@ namespace IIS.SampleForMetrics
             container.LoadConfiguration();
             container.RegisterInstance(DataServiceProvider.DataService);
             GlobalConfiguration.Configure(configuration => ODataConfig.Configure(configuration, container, GlobalConfiguration.DefaultServer));
+
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MessageHandlers.Add(new MetricsWebApiMessageHandler());
+            httpConfiguration.RegisterWebApi();
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            //DEVNOTE: If already using Autofac for example for DI, you would just build the 
+            // servicecollection, resolve IMetrics and register that with your container instead.
+            var provider = services.SetDependencyResolver(httpConfiguration);
+
+            appBuilder.UseMetrics(provider);
+
+            HostingEnvironment.QueueBackgroundWorkItem(cancellationToken =>
+            {
+                var reportFactory = provider.GetRequiredService<IReportFactory>();
+                var metrics = provider.GetRequiredService<IMetrics>();
+                var reporter = reportFactory.CreateReporter();
+                reporter.RunReports(metrics, cancellationToken);
+            });
+
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+
+            services.AddControllersAsServices();
+
+            services
+                .AddMetrics(options =>
+                {
+                    options.DefaultContextLabel = "SampleForMetrics.ODataBackend";
+                    options.ReportingEnabled = true;
+                }, Assembly.GetExecutingAssembly().GetName())
+                .AddHealthChecks(factory =>
+                {
+                    factory.RegisterProcessPrivateMemorySizeHealthCheck("Private Memory Size", 200);
+                    factory.RegisterProcessVirtualMemorySizeHealthCheck("Virtual Memory Size", 200);
+                    factory.RegisterProcessPhysicalMemoryHealthCheck("Working Set", 200);
+                })
+                .AddJsonSerialization()
+                .AddMetricsMiddleware(options =>
+                {
+
+                });
         }
     }
 }
